ea299eae33dc21e9255ec8aa700b054b77e337d9
CibhichakaravarthiCR <92293171+CibhichakaravarthiCR@users.noreply.github.com>
2021-10-11 18:04:57 +0530

    Add files via upload

---
+++ CG day1/src/com/basic/Point2D.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class Point2D {
+	public int x,y,a,b;
+	public Point2D(int a,int b) {
+		x=a;
+		y=b;
+	}
+	public String getDetails() {
+		System.out.println("point Details:"+x+" "+y);
+		return null;
+		
+	}
+}
---
+++ CG day1/src/com/basic/TestPoint.java
@@ -0,0 +1,9 @@
+package com.basic;
+
+public class TestPoint {
+	public static void main(String[] args) {
+		Point2D a1=new Point2D(10,20);
+		a1.getDetails();
+	}
+
+}
